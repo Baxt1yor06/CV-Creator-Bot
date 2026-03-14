6d26ef86f03ed08780a809434115eed5ce906bbc
Bakhtiyor <babduvaliyev180@gmail.com>
2026-03-14 16:03:50 +0500

    V5.3

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import asyncio
 import logging
 import sys
-from aiogram import Bot, Dispatcher
+from typing import Callable, Dict, Any, Awaitable
+from aiogram import Bot, Dispatcher, BaseMiddleware
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.types import TelegramObject
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.context import FSMContext
 from os import getenv
 from dotenv import load_dotenv
+from aiogram.utils.i18n import I18n, FSMI18nMiddleware
 from messages import common, quiz
-import utilits
+
+
 
 
 
@@ -19,6 +24,30 @@ storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
 
+i18n = I18n(path="locales", default_locale="uz", domain="messages")
+i18n_middleware = FSMI18nMiddleware(i18n)
+dp.update.middleware(i18n_middleware)
+
+
+# ===============================
+# LOCALE RESTORE MIDDLEWARE
+# ===============================
+
+class LocaleRestoreMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any],
+    ) -> Any:
+        state: FSMContext = data.get("state")
+        if state:
+            state_data = await state.get_data()
+            locale = state_data.get("locale", "uz")
+            await i18n_middleware.set_locale(state, locale)
+        return await handler(event, data)
+
+dp.update.middleware(LocaleRestoreMiddleware())
 
 
 
@@ -49,7 +78,6 @@ if __name__ == '__main__':
 async def main() -> None:
     dp.include_router(common.router)
     dp.include_router(quiz.router)
-    dp.include_router(utilits.router)
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await dp.start_polling(bot)
 
--- utilits.py
+++ utilits.py
@@ -1,13 +1,7 @@
 from aiogram.fsm.state import State, StatesGroup
 import re
-from aiogram import Router
-from aiogram.utils.i18n import I18n, FSMI18nMiddleware
-from aiogram.fsm.context import FSMContext
-from aiogram.types import TelegramObject
-from aiogram import BaseMiddleware
-from typing import Callable, Dict, Any, Awaitable
 
-router = Router()
+
 
 class UserStates(StatesGroup):
     user_full_name = State()
@@ -42,27 +36,3 @@ def check_year(text: str):
     return re.match(r"^\d{4}\s-\s\d{4}$", text)
 
 
-i18n = I18n(path="locales", default_locale="uz", domain="messages")
-i18n_middleware = FSMI18nMiddleware(i18n)
-router.update.middleware(i18n_middleware)
-
-
-# ===============================
-# LOCALE RESTORE MIDDLEWARE
-# ===============================
-
-class LocaleRestoreMiddleware(BaseMiddleware):
-    async def __call__(
-        self,
-        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-        event: TelegramObject,
-        data: Dict[str, Any],
-    ) -> Any:
-        state: FSMContext = data.get("state")
-        if state:
-            state_data = await state.get_data()
-            locale = state_data.get("locale", "uz")
-            await i18n_middleware.set_locale(state, locale)
-        return await handler(event, data)
-
-router.message.middleware(LocaleRestoreMiddleware())
